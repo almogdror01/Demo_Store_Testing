367f9481637d41013e768632fbf0a1aa69f31f9a
KoralPo <106435061+KoralPo@users.noreply.github.com>
2022-07-19 10:48:26 +0300

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -8,10 +8,11 @@ import org.testng.annotations.Test;
 @Listeners({listeners.class})
 
 public class Tests extends StoreApp {
+
     @Test(description = "Test cash 01.")
     @Description("Test Descriotion: cash 01.")
     public void test01() throws InterruptedException {
-        barElem.barlinksFunctionality();
+        barElem.chooseBarLink(getData("BarLinkName",4));
         accessories.selectFilterAccessories(getData("SortBy",4), driver);
         verify.assertEqualsfun(accessories.getResultCountAccessories(), accessories.getResultCountAccessories());
         accessories.accessoriesProductsName();
