fba0e765b053a4ffaf6fab50bf9a489c2190d5c0
KoralPo <106435061+KoralPo@users.noreply.github.com>
2022-07-19 10:05:03 +0300

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -1,3 +1,19 @@
-public class Tests
-{
+import StorProject.HeaderBarElements;
+import StorProject.StoreApp;
+import StorProject.listeners;
+import io.qameta.allure.Description;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners({listeners.class})
+
+public class Tests extends StoreApp {
+    @Test(description = "Test cash 01.")
+    @Description("Test Descriotion: cash 01.")
+    public void test01() throws InterruptedException {
+        barElem.barlinksFunctionality();
+        accessories.selectFilterAccessories("Sort by price: low to high", driver);
+        verify.assertEqualsfun(accessories.getResultCountAccessories(), accessories.getResultCountAccessories());
+        accessories.accessoriesProductsName();
+    }
 }
