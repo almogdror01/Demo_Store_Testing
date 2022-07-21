e3facdb39490e4df5e596a93eb015bcb29728429
KoralPo <106435061+KoralPo@users.noreply.github.com>
2022-07-21 07:54:56 +0300

    Add files via upload

--- Tests.java
+++ Tests.java
@@ -2,12 +2,20 @@ import StorProject.HeaderBarElements;
 import StorProject.StoreApp;
 import StorProject.listeners;
 import io.qameta.allure.Description;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 @Listeners({listeners.class})
 
 public class Tests extends StoreApp {
+
+    @BeforeClass
+    public void openBrowser()
+    {
+        initialize();
+        getDriver().get(getData("url",0));
+    }
 
     @Test(description = "Test case 01.")
     @Description("Test Descriotion: case 01.")
