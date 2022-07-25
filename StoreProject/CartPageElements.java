45da2f9b1eeb9799812c63ebb33acff4e42b8760
KoralPo <106435061+KoralPo@users.noreply.github.com>
2022-07-25 12:26:24 +0300

    Add files via upload

--- StoreProject/CartPageElements.java
+++ StoreProject/CartPageElements.java
@@ -9,26 +9,26 @@ import static org.testng.Assert.assertEquals;
 public class CartPageElements extends StoreApp
 {
     @FindBy(name = "coupon_code")
-    private WebElement input_coupon;
+    private WebElement input_coupon_Cart;
 
     @FindBy(xpath = "//div[@class='coupon']/button")
-    private WebElement btn_applyCoupon;
+    private WebElement btn_applyCoupon_Cart;
 
     @FindBy(xpath = "//tr[@class='order-total']/td/strong/span/bdi")
-    private WebElement txt_totalPrice;
+    private WebElement txt_totalPrice_Cart;
 
     @FindBy(xpath = "//tr[@class='cart-subtotal']/td/span/bdi")
-    private WebElement txt_subTotalPrice;
+    private WebElement txt_subTotalPrice_Cart;
 
     @FindBy(xpath = "//ul[@class='woocommerce-error']/li")
-    private WebElement txt_errorMessage;
+    private WebElement txt_errorMessage_Cart;
 
 
     @Step("Add Coupon")
     public void addCoupon(String couponName)
     {
-        input_coupon.sendKeys(couponName);
-        btn_applyCoupon.click();
+        input_coupon_Cart.sendKeys(couponName);
+        btn_applyCoupon_Cart.click();
     }
     @Step("Confirm Coupon")
     public void confirmCoupon(String couponName)
@@ -36,7 +36,7 @@ public class CartPageElements extends StoreApp
         if (couponName.equals("example"))
         {
             String errorMessage = "51.00 ₪";
-            assertEquals(txt_totalPrice.getText(),errorMessage,txt_errorMessage.getText());
+            assertEquals(txt_totalPrice_Cart.getText(),errorMessage,txt_errorMessage_Cart.getText());
         }
     }
 
