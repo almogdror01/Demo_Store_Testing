2d260f3455b843c70431b55683b6c62bc9f065d1
KoralPo <106435061+KoralPo@users.noreply.github.com>
2022-07-25 12:12:37 +0300

    Update Tests.java

--- Tests.java
+++ Tests.java
@@ -15,7 +15,7 @@ public class Tests extends StoreApp {
     }
 
     @Test(description = "Test case 01.")
-    @Description("Test Descriotion: case 01.")
+    @Description("Test Description: case 01.")
     public void test01() {
         barElem.chooseBarLink(getData("BarLinkName",4));
         accessories.selectFilterAccessories(getData("SortBy",4));
@@ -25,7 +25,7 @@ public class Tests extends StoreApp {
     }
 
     @Test(description = "Test case 02.")
-    @Description("Test Descriotion: case 02.")
+    @Description("Test Description: case 02.")
     public void test02() {
         homePageElem.clickOnShopNow();
         storePageElem.sliderFilter(50,90);
